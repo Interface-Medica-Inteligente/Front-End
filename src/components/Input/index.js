27ef416a59df58d64efd09476bedf7e2e8cd386b
LuisHenrique7 <luiswar12@gmail.com>
2021-06-10 21:33:53 -0300

    feat: Estrutura da tela de login

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const Input = () => {
+    return(
+        <div className='container-input'>
+            <input className='input-text' placeholder='Text'/>
+        </div>
+    )
+}
+
+export default Input
