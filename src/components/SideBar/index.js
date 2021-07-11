1b2940adb181ac10f5fcb3c9f6c66081354412cb
isaac-oliveira <isaac.oliveira@popcode.com.br>
2021-07-11 20:38:12 -0300

    chore: ajustes na sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useLocation } from 'react-router'
 import { Actions as DoctorActions } from '../../reducers/doctor'
+import attendanceSelector from '../../selectors/attendanceSelector'
 
 import './styles.css'
 
 const SideBar = ({ children }) => {
+  const attendanceId = useSelector(attendanceSelector.getAttendanceId)
   const dispatch = useDispatch()
 
   const handleLogout = () => {
@@ -19,8 +21,8 @@ const SideBar = ({ children }) => {
           <div className='logo' />
           <div className='container-buttons'>
             <Item name='record' title='Prontuário' />
-            <Item name='recipe' title='Receita' />
-            <Item name='report' title='Laudo' />
+            <Item name='recipe' disabled={!attendanceId} title='Receita' />
+            <Item name='report' disabled={!attendanceId} title='Laudo' />
           </div>
         </div>
         <div onClick={handleLogout} className='logout'>
@@ -34,18 +36,20 @@ const SideBar = ({ children }) => {
   )
 }
 
-const Item = ({ name, title }) => {
+const Item = ({ name, title, disabled = false }) => {
   const history = useHistory()
   const { pathname } = useLocation()
 
   const active = `/${name}` === pathname
 
   const handlePush = () => {
-    history.push(name)
+    if (!disabled) {
+      history.push(name)
+    }
   }
 
   return (
-    <div onClick={handlePush} className={`container-button ${active ? 'active' : ''}`}>
+    <div onClick={handlePush} className={`container-button ${active ? 'active' : ''} ${disabled ? 'disabled' : ''}`}>
       <p className='button-title'>{title}</p>
     </div>
   )
